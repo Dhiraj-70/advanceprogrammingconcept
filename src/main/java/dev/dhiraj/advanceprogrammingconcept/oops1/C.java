dd8388ada2301898d3b87ab83c09248959379096
unknown <Global@del1-lhp-n78070.synapse.com>
2025-01-31 23:31:52 +0530

    Super keyword to call parametrize constructor of parent class

--- src/main/java/dev/dhiraj/advanceprogrammingconcept/oops1/C.java
+++ src/main/java/dev/dhiraj/advanceprogrammingconcept/oops1/C.java
@@ -1,7 +1,11 @@
 package dev.dhiraj.advanceprogrammingconcept.oops1;
 
 public class C extends B{
-    public C(){
-        System.out.println("C constructor");
+//    public C(){
+//        System.out.println("C constructor");
+//    }
+
+    public C(String a){
+        System.out.println("C constructor with 1 parameter");
     }
 }
--- src/main/java/dev/dhiraj/advanceprogrammingconcept/oops1/D.java
+++ src/main/java/dev/dhiraj/advanceprogrammingconcept/oops1/D.java
@@ -2,6 +2,7 @@ package dev.dhiraj.advanceprogrammingconcept.oops1;
 
 public class D extends C {
     public D(){
+        super("First Parent is C");
         System.out.println("D constructor");
     }
 }
