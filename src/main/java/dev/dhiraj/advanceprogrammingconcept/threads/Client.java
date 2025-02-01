e6d8cc352bcd8616f7f528ce303da709f10ad808
unknown <Global@del1-lhp-n78070.synapse.com>
2025-02-01 09:13:38 +0530

    Thread

---
+++ src/main/java/dev/dhiraj/advanceprogrammingconcept/threads/Client.java
@@ -0,0 +1,7 @@
+package dev.dhiraj.advanceprogrammingconcept.threads;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Thread printed by: "+ Thread.currentThread().getName());
+    }
+}
