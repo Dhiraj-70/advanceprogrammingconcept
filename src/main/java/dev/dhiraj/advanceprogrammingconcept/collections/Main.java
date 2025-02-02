f6f03027733c9a63087085b1c19e3f59a2221c68
unknown <Global@del1-lhp-n78070.synapse.com>
2025-02-02 13:08:33 +0530

    Set Interface

--- src/main/java/dev/dhiraj/advanceprogrammingconcept/collections/Main.java
+++ src/main/java/dev/dhiraj/advanceprogrammingconcept/collections/Main.java
@@ -1,12 +1,10 @@
 package dev.dhiraj.advanceprogrammingconcept.collections;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 
 public class Main {
     public static void main (String[] args){
+        //List Interface
         List<Integer> list = new ArrayList<>(); // LinkedList, Vector
         list.add(44);
         list.add(1);
@@ -14,10 +12,26 @@ public class Main {
         list.add(33);
         list.add(44);
 
-        System.out.println("size: "+list.size());
+        System.out.println("list size: "+list.size());
         for(Integer l : list){
             System.out.print(l+" ");
         }
+        System.out.println("\n2nd index element: "+list.get(1));
 
+        //Set Interface
+        Set<String> set = new HashSet<>();//LinkedHashSet, TreeSet
+        set.add("India");
+        set.add("China");
+        set.add("France");
+        set.add("USA");
+        set.add("India");
+        set.add("China");
+        set.add(null);//NullPointerException if TreeSet is uesd for Obj Creation
+        set.add(null);
+
+        System.out.println("\nset size: "+set.size());
+        for(String s : set){
+            System.out.print(s+" ");
+        }
     }
 }
